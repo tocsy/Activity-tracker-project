811696208ba258d97ad226ebac0c33a825be658c
Horváth Bátor <batorko@gmail.com>
2018-07-27 10:21:18 +0200

    asd

---
+++ src/main/java/activity/Track.java
@@ -0,0 +1,26 @@
+package activity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Track {
+
+    List<> trackPoints = new ArrayList();
+
+    public void addTrackPoint(){}
+
+    public Coordinate findMaximumCoordinate(){}
+
+    public Coordinate findMinimumCoordinate(){}
+
+    public double getDistance(){}
+
+    public double getFullDecrease(){}
+
+    public double getFullElevation(){}
+
+    public double getRectangleArea(){}
+
+
+}
